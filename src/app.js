79a34097cb264cb22b2a2958ed3ee9875fa9f136
Abraao4raujo <a68816619@gmail.com>
2023-12-12 12:00:22 -0300

    add gitignore

---
+++ src/app.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = 8081;
+const hora = new Date();
+
+const path = require("path");
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+const eNews = mongoose.model("eNews", {
+  title: String,
+  description: String,
+  img_url: String,
+  auth: String,
+  data_posted: Date,
+});
+
+app.get("/", async (req, res) => {
+  res.sendFile(path.join(__dirname, "./public", "index.html"));
+  // res.json({ hello: "hello" });
+});
+
+app.get("/news", async (req, res) => {
+  const news = await eNews.find();
+  res.send(news);
+});
+
+app.get("/news/:id", async (req, res) => {
+  const news = await eNews.findById(req.params.id);
+  res.send(news);
+});
+
+app.delete("/news/delete/:id", async (req, res) => {
+  const newsDelete = await eNews.findByIdAndDelete(req.params.id);
+  res.send(newsDelete);
+});
+
+app.post("/sendNews", async (req, res) => {
+  const news = new eNews({
+    title: req.body.title,
+    description: req.body.description,
+    img_url: req.body.img_url,
+    auth: req.body.auth,
+    data_posted: hora,
+  });
+
+  news.save().then(() => res.send(news));
+});
+
+app.listen(PORT, () => {
+  mongoose.connect(
+    "mongodb+srv://abraao:DaSqmNQQNg9Kuxsz@enews.32kfydx.mongodb.net/?retryWrites=true&w=majority"
+  );
+  console.log("server running at port: ", PORT);
+});
